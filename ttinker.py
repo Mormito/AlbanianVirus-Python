33ae97b24f015eabcc30981b325485bad22f94d8
Guilherme Mormito <guimormis@gmail.com>
2024-10-18 13:51:48 -0300

    update

---
+++ ttinker.py
@@ -0,0 +1,14 @@
+import os
+import platform
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+def albanianVirus():
+    messagebox.showerror("Virus Alert!", 
+    "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me other users. Many thanks for your cooperation! Best regards, Albanian virus")
+
+    
+
+albanianVirus()
